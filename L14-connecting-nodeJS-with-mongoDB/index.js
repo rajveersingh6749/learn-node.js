23fe5e2861beafc7a58685067d9856807d2e66c8
Rajveer Singh <777rajveersingh@gmail.com>
2025-10-25 17:40:42 +0530

    add logging middleware for request tracking

--- L14-connecting-nodeJS-with-mongoDB/index.js
+++ L14-connecting-nodeJS-with-mongoDB/index.js
@@ -6,44 +6,43 @@ const fs = require("fs");
 const app = express();
 const PORT = 8000;
 
-mongoose.
-connect("mongodb://127.0.0.1:27017/youtube-app-1")
-.then(() => {
-  console.log("MongoDB Connected");
-})
-.catch((err) => {
-  console.log("MongoDB Connection Error", err);
-});
+mongoose
+  .connect("mongodb://127.0.0.1:27017/youtube-app-1")
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Error", err);
+  });
 
 const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    jobTitle: {
-        type: String,
-    },
-    gender: {
-        type: String,
-    }
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  jobTitle: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
 
 app.use(express.urlencoded({ extended: false })); // to parse form data
 
-
 app.post("/api/users", async (req, res) => {
   const body = req.body;
-  if(
+  if (
     !body ||
     !body.firstName ||
     !body.lastName ||
@@ -51,7 +50,7 @@ app.post("/api/users", async (req, res) => {
     !body.gender ||
     !body.jobTitle
   ) {
-    return res.status(400).json({ msg: "All fields are required..."});
+    return res.status(400).json({ msg: "All fields are required..." });
   }
 
   const result = await User.create({
@@ -64,15 +63,17 @@ app.post("/api/users", async (req, res) => {
 
   console.log("result:", result);
 
-  return res.status(201).json({msg: "Success"});
-})
+  return res.status(201).json({ msg: "Success" });
+});
 
 // Routes
 app.get("/users", async (req, res) => {
-    const allDbUsers = await User.find({});
+  const allDbUsers = await User.find({});
   const html = `
   <ul>
-    ${allDbUsers.map((user) => `<li>${user.firstName} - ${user.email}</li>`).join('')}
+    ${allDbUsers
+      .map((user) => `<li>${user.firstName} - ${user.email}</li>`)
+      .join("")}
   </ul>
   `;
 
@@ -81,7 +82,7 @@ app.get("/users", async (req, res) => {
 
 // REST API
 app.get("/api/users", async (req, res) => {
-    const allDbUsers = await User.find({});
+  const allDbUsers = await User.find({});
 
   return res.json(allDbUsers);
 });
@@ -90,13 +91,13 @@ app
   .route("/api/users/:id")
   .get(async (req, res) => {
     const user = await User.findById(req.params.id);
-    if(!user) {
-        return res.status(404).json({ msg: "User not found..."});
+    if (!user) {
+      return res.status(404).json({ msg: "User not found..." });
     }
     return res.json(user);
   })
   .patch(async (req, res) => {
-    await User.findByIdAndUpdate(req.params.id,{ lastName: "Changed" });
+    await User.findByIdAndUpdate(req.params.id, { lastName: "Changed" });
     return res.json({ status: "Success" });
   })
   .delete(async (req, res) => {
@@ -104,7 +105,15 @@ app
     return res.json({ status: "Success" });
   });
 
-
+app.use((req, res, next) => {
+  fs.appendFile(
+    "log.txt",
+    `\n${Date.now()}:${req.ip} ${req.method}: ${req.path}\n`,
+    (err, data) => {
+      next();
+    }
+  );
+});
 
 app.listen(PORT, () => {
   console.log("Server Started at PORT", PORT);
